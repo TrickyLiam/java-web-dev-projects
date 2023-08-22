3f9df6f0f7884dc4490cb7f2e75d7ab009cce564
TrickyLiam <liamcarlson2014@gmail.com>
2023-08-21 20:13:06 -0500

    fix broken loop, finish studio

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,14 +6,14 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the radius of a circle: ");
+
         double radius = input.nextDouble();
 
-        do {
-            if (radius < 0) {
-                System.out.println("Please enter a postive number only");
-            }
-        } while (radius < 0);
-        
+        while (radius < 0) {
+            System.out.println("Please enter a positive number for the radius: ");
+            radius = input.nextDouble();
+        }
+
         double area = Circle.getArea(radius);
 //        double area = 3.14 * radius * radius;
         System.out.println("The area of this circle is " + area);
