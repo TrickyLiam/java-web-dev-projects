995af2e1674d84553124a4240280d17bc08bc733
TrickyLiam <liamcarlson2014@gmail.com>
2023-08-23 13:46:17 -0500

    create exercises

---
+++ control-flow-and-collections/exercises/collections-exercises/arrayPractice.java
@@ -0,0 +1,15 @@
+public class arrayPractice {
+
+    public static void main(String[] args) {
+
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+
+        for (int i = 0; i < integerArray.length; i++) {
+            System.out.println(integerArray[i]);
+        }
+
+    }
+
+
+
+}
